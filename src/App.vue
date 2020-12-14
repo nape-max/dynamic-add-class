1288ae8d7dba72230ba014b7570acc7a3bf979f8
nape-max <asus@example.com>
2020-12-14 09:55:22 +0400

    Add dymanic class change, dynamic 'disabled' input range. Some refactoring code.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,73 @@
 <template>
   <div id="app">
-    <input type="file" @change="imageChange"/>
-    <img alt="Image Preview" v-if="filePath" :src="filePath" id="image-preview">
+    <div id="photo-editor">
+      <div id="preview">
+        <img
+          alt="Image Preview"
+          v-if="filePath && isDisplayImage"
+          :src="filePath"
+          :style="imageSizeStyles"
+          :class="imageClasses"
+          id="image-preview"
+        />
+      </div>
+      <div id="control-buttons">
+        <input 
+          type="file"
+          @change="imageChange"
+        />
+        <div id="style-buttons">
+          <input 
+            type="button"
+            :class="imageClasses.border ? 'active' : ''"
+            @click="changeBorderClass"
+            value="Рамка"
+          />
+          <input 
+            type="button"
+            :class="imageClasses.shadow ? 'active' : ''"
+            @click="changeShadowClass"
+            value="Тень"
+          />
+          <input
+            type="button"
+            :class="imageClasses.small ? 'active' : ''"
+            @click="changeSmallClass"
+            value="Уменьшить размер"
+          />
+        </div>
+        <div class="input-range-flex">
+          <label for="inputWidth">Ширина: {{ imageSizes.currentWidth }}</label>
+          <input 
+            id="inputWidth"
+            type="range"
+            min="0"
+            :max="imageSizes.maxWidth"
+            :value="imageSizes.currentWidth"
+            :disabled="imageClasses.small ? true : false"
+            @input="imageSizes.currentWidth = $event.target.value"
+          />
+        </div>
+        <div class="input-range-flex">
+          <label for="inputHeight">Высота: {{ imageSizes.currentHeight }}</label>
+          <input
+            id="inputHeight"
+            type="range"
+            min="0"
+            :max="imageSizes.maxHeight"
+            :value="imageSizes.currentHeight"
+            :disabled="imageClasses.small ? true : false"
+            @input="imageSizes.currentHeight = $event.target.value"
+          />
+        </div>
+        <input
+          type="button"
+          :class="!isDisplayImage ? 'active' : ''"
+          value="Скрыть изображение"
+          @click="imageHideShow"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,12 +79,47 @@ export default {
   data: function() {
     return {
       filePath: '',
+      imageSizes: {
+        maxWidth: 300,
+        maxHeight: 200,
+        currentWidth: 300,
+        currentHeight: 200,
+      },
+      imageClasses: {
+        border: false,
+        shadow: false,
+        small: false,
+      },
+      isDisplayImage: true,
+    }
+  },
+  computed: {
+    imageSizeStyles() {
+      return {
+        width: `${this.imageSizes.currentWidth}px`,
+        height: `${this.imageSizes.currentHeight}px`
+      };
     }
   },
   methods: {
     imageChange(event) {
       this.filePath = URL.createObjectURL(event.target.files[0]);
     },
+    changeBorderClass() {
+      this.imageClasses.border = !this.imageClasses.border;
+      console.log(this.imageClasses)
+    },
+    changeShadowClass() {
+      this.imageClasses.shadow = !this.imageClasses.shadow;
+    },
+    changeSmallClass(event) {
+      this.imageClasses.small = !this.imageClasses.small;
+      event.target.value = this.imageClasses.small ? "Увеличить размер" : "Уменьшить размер";
+    },
+    imageHideShow(event) {
+      this.isDisplayImage = !this.isDisplayImage;
+      event.target.value = this.isDisplayImage ? "Скрыть изображение" : "Показать изображение";
+    }
   },
   components: {
   }
@@ -36,12 +137,66 @@ export default {
 
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
 }
 
+#photo-editor {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  width: 600px;
+  height: 200px;
+}
+
+#preview {
+  width: 50%;
+  height: 100%;
+}
+
 #image-preview {
-  width: 300px;
-  height: auto;
+  width: 100%;
+  height: 100%;
+}
+
+#control-buttons {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  text-align: left;
+
+  height: 100%;
+  width: 45%;
+
+  padding: 0 10px;
+}
+
+#style-buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.input-range-flex {
+  display: flex;
+  flex-direction: column;
+}
+
+.border {
+  border: 2px dashed black;
+}
+
+.shadow {
+  box-shadow: 5px 5px 8px rgba(0,0,0, 0.5);
+}
+
+.small {
+  width: 60% !important;
+  height: 55% !important;
+}
+
+.active {
+  background-color: #bbb;
 }
 </style>
